17d0063692ac557f1c3b8d37b8a89e908f80d378
Atik Fahad <fahadatik@gmail.com>
2020-04-01 03:38:01 +0600

    add hero image without compression

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,20 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Folks</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
-      <h2>This is my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-        // <pre>{JSON.stringify(post, null, 2)}</pre>
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>This is my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+          // <pre>{JSON.stringify(post, null, 2)}</pre>
+        ))}
+      </Layout>
+    </>
   );
 };
