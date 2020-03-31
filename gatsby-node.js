74a892ea8655efa5ff6bf18652b5e5be224ef4af
Atik Fahad <fahadatik@gmail.com>
2020-04-01 02:04:24 +0600

    add gatsby-node and template for page rendering

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panic('failed to create posts', result.errors);
+  }
+
+  const post = result.data.allMdx.nodes;
+
+  post.forEach(post => {
+    actions.createPage({
+      path: post.frontmatter.slug,
+      component: require.resolve('./src/templates/post.js'),
+      context: {
+        slug: post.frontmatter.slug,
+      },
+    });
+  });
+};
